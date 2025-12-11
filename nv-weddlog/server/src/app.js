e5ea02f7dcb7c926dd94d1389a55f0f48609b95e
- <->
2025-12-11 17:05:32 +0700

    My first upload

--- nv-weddlog/server/src/app.js
+++ nv-weddlog/server/src/app.js
@@ -1,29 +1,13 @@
-let express = require('express');
-const app = express();
+let express = require('express')
+const app = express()
 
-app.get('/status', (req, res) => {
-    res.send('Hello Node.js Server!')
-});
-
-app.get('/hello/:person', function (req, res) {
-    console.log('hello - ' + req.params.person)
-    res.send('say hello with ' + req.params.person)
-});
-
-// get user by id
-app.get('/user/:userId', function (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน')
+// สร้าง route สำหรับหน้า status
+app.get('/status', function (req, res) {
+    res.send('Hello nodejs server')
 })
-// get all user
-app.get('/users', function (req, res) {
-    res.send('เรียกข้อมูลผู้ใช้งาน')
-})
-// get all user
-app.get('/users', function (req, res) {
-    res.send('เรียกข้อมูลผู้ใช้งานทั้งหมด')
-})
-let port = process.env.PORT || 8081;
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// กำหนด port เป็น 8081
+let port = 8081
+app.listen(port, function () {
+    console.log('server running on ' + port)
+})
